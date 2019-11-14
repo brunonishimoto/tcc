3cfa4482e272af0529ccfe76f5a50b59b7fb8105
Bruno Nishimoto <bruno_nishimoto@hotmail.com>
2019-11-14 12:25:55 -0300

    assign probability to rank actions

--- dialogue_system/dm/dst/belief_state_tracker_new.py
+++ dialogue_system/dm/dst/belief_state_tracker_new.py
@@ -277,23 +277,23 @@ class BeliefStateTrackerNew:
         """
 
         n_best_actions = self.__generate_noise_user_actions(user_action)
-        # ranked_actions = self.__rank_actions(n_best_actions)
-        for idx, action in enumerate(n_best_actions):
+        ranked_actions = self.__rank_actions(n_best_actions)
+        for idx, action in enumerate(ranked_actions):
             for key, value in action[const.INFORM_SLOTS].items():
                 self.current_informs[idx][key] = value
         user_action.update({const.ROUND: self.round_num + 1, const.SPEAKER_TYPE: const.USR_SPEAKER_VAL})
-        self.history.append(n_best_actions)
+        self.history.append(ranked_actions)
         self.round_num += 1
 
     def __generate_noise_user_actions(self, user_action):
         n_best_confused_actions = []
-        if np.random.random() <= 1 - self.error_prob:
-            n_best_confused_actions.append(user_action)
-            n_best_confused_actions.append(cfg.correct)
-        else:
-            n_best_confused_actions.append(cfg.correct)
-            n_best_confused_actions.append(user_action)
-        for i in range(2, self.n_best):
+        # if np.random.random() <= 1 - self.error_prob:
+        #     n_best_confused_actions.append(user_action)
+        #     n_best_confused_actions.append(cfg.correct)
+        # else:
+        n_best_confused_actions.append(cfg.correct)
+        n_best_confused_actions.append(user_action)
+        for _ in range(2, self.n_best):
             n_best_confused_actions.append(self.__create_wrong_action(user_action))
 
         # choice = np.random.randint(1, self.n_best)
@@ -395,59 +395,63 @@ class BeliefStateTrackerNew:
         return action
 
     def __rank_actions(self, n_best_actions):
-        scores = []
+        scores = [1 - self.error_prob, (2 * self.error_prob) / 3, self.error_prob / 3]
 
-        last_agent_action = self.history[-1] if len(self.history) > 1 else None
+        ranked_actions = np.random.choice(n_best_actions, len(n_best_actions), replace=False, p=scores)
+
+        return ranked_actions
+
+        # last_agent_action = self.history[-1] if len(self.history) > 1 else None
 
 
-        for action in n_best_actions:
-            score = 0
-            for i in range(self.n_best):
-                db_results_dict = self.db_helper.get_db_results_for_slots(self.current_informs[i])
+        # for action in n_best_actions:
+        #     score = 0
+        #     for i in range(self.n_best):
+        #         db_results_dict = self.db_helper.get_db_results_for_slots(self.current_informs[i])
 
-                new_current_informs = copy.deepcopy(self.current_informs[i])
+        #         new_current_informs = copy.deepcopy(self.current_informs[i])
 
-                # Penalize repeated infomation
-                for key, value in action[const.INFORM_SLOTS].items():
-                    if key in self.current_informs:
-                        if self.current_informs[key] == value:
-                            score -= 1.5
-                        else:
-                            score -= 1
-                    new_current_informs[key] = value
+        #         # Penalize repeated infomation
+        #         for key, value in action[const.INFORM_SLOTS].items():
+        #             if key in self.current_informs:
+        #                 if self.current_informs[key] == value:
+        #                     score -= 1.5
+        #                 else:
+        #                     score -= 1
+        #             new_current_informs[key] = value
 
-                new_db_results_dict = self.db_helper.get_db_results_for_slots(new_current_informs)
+        #         new_db_results_dict = self.db_helper.get_db_results_for_slots(new_current_informs)
 
-                # If there are no matches on db with this action
-                if action[const.INTENT] == const.INFORM:
-                    if new_db_results_dict[const.KB_MATCHING_ALL_CONSTRAINTS] == 0:
-                        score -= 1.0
-                    else:
-                        score += 1.0
-                elif action[const.INTENT] == const.REQUEST:
-                    for key, value in action[const.REQUEST_SLOTS].items():
-                        if key in self.current_informs:
-                            score -=1.0
+        #         # If there are no matches on db with this action
+        #         if action[const.INTENT] == const.INFORM:
+        #             if new_db_results_dict[const.KB_MATCHING_ALL_CONSTRAINTS] == 0:
+        #                 score -= 1.0
+        #             else:
+        #                 score += 1.0
+        #         elif action[const.INTENT] == const.REQUEST:
+        #             for key, value in action[const.REQUEST_SLOTS].items():
+        #                 if key in self.current_informs:
+        #                     score -=1.0
 
-                # Score based on the last agent action
-                if last_agent_action:
-                    if last_agent_action[const.INTENT] == const.INFORM_SLOTS:
-                        if action[const.INTENT] == const.REQUEST:
-                            score += 1.0
-                        elif action[const.INTENT] == const.INFORM:
-                            score += 0.5
-                        else:
-                            score -= 0.5
-                    elif last_agent_action[const.INTENT] == const.REQUEST:
-                        if action[const.INTENT] == const.INFORM:
-                            score += 1.0
-                        elif action[const.INTENT] == const.REQUEST:
-                            score += 0.5
-                        elif action[const.INTENT] == const.THANKS:
-                            score += 0.1
-                        else:
-                            score -= 0.5
+        #         # Score based on the last agent action
+        #         if last_agent_action:
+        #             if last_agent_action[const.INTENT] == const.INFORM_SLOTS:
+        #                 if action[const.INTENT] == const.REQUEST:
+        #                     score += 1.0
+        #                 elif action[const.INTENT] == const.INFORM:
+        #                     score += 0.5
+        #                 else:
+        #                     score -= 0.5
+        #             elif last_agent_action[const.INTENT] == const.REQUEST:
+        #                 if action[const.INTENT] == const.INFORM:
+        #                     score += 1.0
+        #                 elif action[const.INTENT] == const.REQUEST:
+        #                     score += 0.5
+        #                 elif action[const.INTENT] == const.THANKS:
+        #                     score += 0.1
+        #                 else:
+        #                     score -= 0.5
 
-            scores.append(score)
+        #     scores.append(score)
 
-        return np.flip(np.array(n_best_actions).take(np.argsort(scores)))
+        # return np.flip(np.array(n_best_actions).take(np.argsort(scores)))
